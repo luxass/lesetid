3394292341bd865ec4ebb3c2c0cae97ef49ccd8f
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-10-15 20:19:32 +0200

    fix: use darkmode on error page

--- www/error.vue
+++ www/error.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+useDark();
 useHead({
   title: "lesetid | playground",
 });
@@ -12,14 +13,14 @@ useSeoMeta({
 
 <template>
   <div class="h-100dvh flex gap-4 flex-col items-center justify-center">
-    <h1 class="text-8xl text-blue-500">
+    <h1 class="text-8xl text-blue-600">
       404
     </h1>
     <p class="mb-8 text-2xl">
       Page not found
     </p>
-    <Link href="/">
+    <NuxtLink href="/">
       Go back
-    </Link>
+    </NuxtLink>
   </div>
 </template>
