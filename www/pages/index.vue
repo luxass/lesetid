63f4a4961d562c5e3a28cac6a8947e4a753f47d6
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-10-16 16:58:02 +0200

    fix: center dark toggle

--- www/pages/index.vue
+++ www/pages/index.vue
@@ -15,11 +15,11 @@ const text = ref("");
       <NuxtLink href="https://github.com/luxass/lesetid">
         <Icon name="octicon:mark-github" size="24" />
       </NuxtLink>
-      <NuxtLink href="https://npmjs.com/package/lesetid">
+      <!-- <NuxtLink href="https://npmjs.com/package/lesetid">
         <Icon name="carbon:logo-npm" size="24" />
-      </NuxtLink>
+      </NuxtLink> -->
 
-      <button title="Toggle Dark Mode" class="ml1 text-lg op-50 hover:op-75" @click="toggleDark()">
+      <button title="Toggle Dark Mode" class="ml1 text-lg op-50 hover:op-75 flex items-center justify-center" @click="toggleDark()">
         <ClientOnly>
           <Icon :name="isDark ? 'carbon:sun' : 'carbon:moon'" size="24" />
 
