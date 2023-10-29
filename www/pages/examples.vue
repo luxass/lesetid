552803be8e896e2073c532483ca6746610dffcd5
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-10-29 05:35:33 +0100

    chore(www): added issue link

--- www/pages/examples.vue
+++ www/pages/examples.vue
@@ -13,6 +13,8 @@ const {
   data,
   error,
 } = await useFetch<{ examples: Example[] }>("https://examples.lesetid.dev");
+
+const issueUrl = "https://github.com/luxass/lesetid/issues/new?assignees=&labels=example&projects=&template=missing-example.yml&title=missing+example+for+%5BEXAMPLE%5D";
 </script>
 
 <template>
@@ -25,16 +27,21 @@ const {
         {{ data?.examples?.length }} examples
       </span>
     </h2>
-    <p>Pick the examples you want, or you can create an issue</p>
+    <p>
+      Pick the examples you want, or you can create an <NuxtLink
+        :href="issueUrl" target="_blank"
+        rel="noopener noreferrer" class="underline underline-offset-3 underline-dashed"
+      >
+        issue.
+      </NuxtLink>
+    </p>
     <section v-if="!error || data?.examples" id="examples" class="grid grid-cols-1 mt-4 gap-4 sm:grid-cols-3 sm:gap-3">
       <ExampleCard
-        v-for="example in data?.examples"
-        :key="example.url" :big="true" :icon-url="example.iconUrl" :title="example.title"
-        :url="example.url"
+        v-for="example in data?.examples" :key="example.url" :big="true" :icon-url="example.iconUrl"
+        :title="example.title" :url="example.url"
       />
       <NuxtLink
-        href="https://github.com/luxass/lesetid/issues/new?assignees=&labels=example&projects=&template=missing-example.yml&title=missing+example+for+%5BEXAMPLE%5D"
-        target="_blank" rel="noopener noreferrer"
+        :href="issueUrl" target="_blank" rel="noopener noreferrer"
         class="border border-base border-dashed hover:border-solid rounded flex-col p-8 flex justify-center items-center gap-2"
       >
         <Icon name="material-symbols:crop-square-outline" size="24" />
