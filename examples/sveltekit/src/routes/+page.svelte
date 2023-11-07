1217a072b97cf5ee08f7f31ecc72d636d5e7de54
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-07 13:14:19 +0100

    chore: renamed examples

---
+++ examples/sveltekit/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <h1>
+    to your new<br />SvelteKit app
+  </h1>
+
+  <h2>
+    try editing <strong>src/routes/+page.svelte</strong>
+  </h2>
+
+</section>
