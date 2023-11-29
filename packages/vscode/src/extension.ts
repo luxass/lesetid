8bc59611e3b717bcbd7f3ff7f8e24214046b4eca
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-29 06:47:38 +0100

    feat: add basic vscode extension

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,10 +1,37 @@
-import type { ExtensionContext } from "vscode";
-import { commands, window, workspace } from "vscode";
+import type { ExtensionContext, StatusBarItem } from "vscode";
+import { StatusBarAlignment, commands, window, workspace } from "vscode";
+import { estimate } from "lesetid";
+import { config } from "./configuration";
+import { log } from "./log";
 
 export function activate(context: ExtensionContext) {
+  let statusBar: StatusBarItem;
+
+  log.info("Reading Time activated", config.get("enabled"));
+
+  if (config.get("enabled")) {
+    statusBar = window.createStatusBarItem(
+      "vscode-reading-time",
+      StatusBarAlignment.Left,
+      2,
+    );
+  }
+
   context.subscriptions.push(
     commands.registerCommand("reading-time.estimate", async () => {
       window.showInformationMessage("Not implemented yet");
+    }),
+  );
+
+  context.subscriptions.push(
+    commands.registerCommand("reading-time.toggle", async () => {
+      const isEnabled = config.get("enabled");
+      const toggled = !isEnabled;
+      await config.set("enabled", toggled);
+
+      if (!toggled) {
+        statusBar.hide();
+      }
     }),
   );
 
@@ -14,22 +41,81 @@ export function activate(context: ExtensionContext) {
 
       const document = editor.document;
 
-      // const text = document.getText();
-      console.error("window#onDidChangeActiveTextEditor", document.languageId);
+      if (!config.get("enabled")) {
+        statusBar.hide();
+        return;
+      }
+
+      const text = document.getText();
+
+      const files = config.get("files");
+      console.log(files, document.languageId);
+      if (files.includes(document.languageId)) {
+        const estimation = estimate(text, {
+          charsPerMinute: config.get("cpm"),
+          wordsPerMinute: config.get("wpm"),
+        });
+
+        statusBar.text = `$(book) ${estimation.text}`;
+        statusBar.show();
+      } else {
+        statusBar.hide();
+      }
     }),
     workspace.onDidChangeTextDocument((event) => {
       const document = event.document;
 
-      // const text = document.getText();
+      if (!config.get("enabled")) {
+        statusBar.hide();
+        return;
+      }
+      const text = document.getText();
 
-      console.error("workspace#onDidChangeTextDocument", document.languageId);
+      const files = config.get("files");
+      console.log(files, document.languageId);
+      if (files.includes(document.languageId)) {
+        const estimation = estimate(text, {
+          charsPerMinute: config.get("cpm"),
+          wordsPerMinute: config.get("wpm"),
+        });
+
+        statusBar.text = `$(book) ${estimation.text}`;
+        statusBar.show();
+      } else {
+        statusBar.hide();
+      }
     }),
     workspace.onDidOpenTextDocument((document) => {
       if (!document) return;
-      console.error("workspace#onDidOpenTextDocument", document, document.languageId);
+      if (!config.get("enabled")) {
+        statusBar.hide();
+        return;
+      }
+      const text = document.getText();
+
+      const files = config.get("files");
+      console.log(files, document.languageId);
+      if (files.includes(document.languageId)) {
+        const estimation = estimate(text, {
+          charsPerMinute: config.get("cpm"),
+          wordsPerMinute: config.get("wpm"),
+        });
+
+        statusBar.text = `$(book) ${estimation.text}`;
+        statusBar.show();
+      } else {
+        statusBar.hide();
+      }
     }),
     workspace.onDidChangeConfiguration((event) => {
-      console.error("workspace#onDidChangeConfiguration", event);
+      if (!event.affectsConfiguration("reading-time")) {
+        return;
+      }
+      const enabled = config.get("enabled");
+      if (!enabled) {
+        statusBar.hide();
+      }
+      log.info(`Configuration changed. Reading time is now ${enabled ? "enabled" : "disabled"}`);
     }),
   );
 }
