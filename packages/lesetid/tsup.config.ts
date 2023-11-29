fba29a6f1b6ebc4a5bbf9f18b6d31dbd468ee93b
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-29 15:50:32 +0100

    chore: lint

--- packages/lesetid/tsup.config.ts
+++ packages/lesetid/tsup.config.ts
@@ -1,30 +1,15 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    entry: ["./src/index.ts", "./src/stream.ts", "./src/utils.ts"],
-    format: ["cjs", "esm"],
-    clean: true,
-    dts: true,
-    treeshake: true,
-    bundle: true,
-    outExtension(ctx) {
-      return {
-        js: ctx.format === "cjs" ? ".cjs" : ".mjs",
-      };
-    },
+export default defineConfig({
+  entry: ["./src/index.ts", "./src/stream.ts", "./src/utils.ts"],
+  format: ["cjs", "esm"],
+  clean: true,
+  dts: true,
+  treeshake: true,
+  bundle: true,
+  outExtension(ctx) {
+    return {
+      js: ctx.format === "cjs" ? ".cjs" : ".mjs",
+    };
   },
-  {
-    entry: ["./src/cli.ts"],
-    format: ["esm"],
-    clean: true,
-    dts: true,
-    treeshake: true,
-    bundle: true,
-    outExtension(ctx) {
-      return {
-        js: ctx.format === "cjs" ? ".cjs" : ".mjs",
-      };
-    },
-  },
-]);
+});
