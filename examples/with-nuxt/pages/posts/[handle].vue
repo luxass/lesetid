73dfe29fdf6c2a5361634bba32254b5e07e110a9
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-02-03 16:41:27 +0100

    fix(nuxt-example): set empty slot in content renderer

--- examples/with-nuxt/pages/posts/[handle].vue
+++ examples/with-nuxt/pages/posts/[handle].vue
@@ -12,13 +12,18 @@ const handle = Array.isArray(route.params.handle)
   <ContentDoc v-slot="{ doc }" :path="handle">
     <article class="tracking-wide prose dark:prose-invert markdown">
       <div class="border border-neutral-300 dark:border-neutral-700 rounded p-3 gap-3">
-        This post will approximately take <strong>{{ (doc as ContentWithEstimation).estimation?.text || "NaN" }}</strong> to read.
+        This post will approximately take <strong>{{ (doc as ContentWithEstimation).estimation?.text || "NaN" }}</strong>
+        to read.
       </div>
 
       <h1 class="mt-8">
         {{ doc.title }}
       </h1>
-      <ContentRenderer :value="doc" />
+      <ContentRenderer :value="doc">
+        <template #empty>
+          <p>No content found.</p>
+        </template>
+      </ContentRenderer>
     </article>
   </ContentDoc>
 </template>
