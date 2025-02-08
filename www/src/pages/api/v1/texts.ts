e46d5b2008d6bd577d8a7ea34506f06af0efbb99
Lucas <lucasnrgaard@gmail.com>
2025-02-08 04:59:52 +0100

    feat: add support for texts

--- www/src/pages/api/v1/texts.ts
+++ www/src/pages/api/v1/texts.ts
@@ -1,5 +1,29 @@
+import { join } from "node:path";
+
 export async function GET() {
-  return new Response(JSON.stringify([]), {
+  const fileObjects = Object.values(import.meta.glob("../../../texts/*.md", { eager: true }));
+
+  const files = await Promise.all(fileObjects.map(async (fileModule) => {
+    if (
+      fileModule == null
+      || typeof fileModule !== "object"
+      || (!("file" in fileModule) || typeof fileModule.file !== "string")
+      || (!("rawContent" in fileModule) || typeof fileModule.rawContent !== "function")
+    ) {
+      return null;
+    }
+
+    if (fileModule.file.endsWith("README.md")) {
+      return null;
+    }
+
+    return {
+      file: fileModule.file.replace(join(import.meta.dirname, "../../../texts/"), ""),
+      content: fileModule.rawContent(),
+    };
+  }));
+
+  return new Response(JSON.stringify(files.filter(Boolean)), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
