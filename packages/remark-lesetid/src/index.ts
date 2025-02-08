66402b01a1662b3479ef4cf8c25e7a04eb22ad1b
Lucas <lucasnrgaard@gmail.com>
2025-02-08 08:57:44 +0100

    fix(remark-lesetid): extend options with lesetid options

--- packages/remark-lesetid/src/index.ts
+++ packages/remark-lesetid/src/index.ts
@@ -1,11 +1,11 @@
 import type { Root } from "mdast";
 import type { Plugin } from "unified";
-import { estimate, type Estimation } from "lesetid";
+import { estimate, type Estimation, type Options as LesetidOptions } from "lesetid";
 import { visit } from "unist-util-visit";
 
-export type { Estimation } from "lesetid";
+export type { Estimation };
 
-export interface Options {
+export interface Options extends LesetidOptions {
   dataKey?: string;
 }
 
@@ -19,14 +19,15 @@ const remarkLesetid: Plugin<Options[], Root> = (options) => {
     throw new Error("Missing dataKey");
   }
 
-  const { dataKey } = options;
+  const { dataKey, ...rest } = options;
+
   return (tree, file) => {
     let textOnPage = "";
     visit(tree, "text", (node) => {
       textOnPage += node.value;
     });
 
-    const estimation = estimate(textOnPage);
+    const estimation = estimate(textOnPage, rest);
     // if matter is defined in data, the frontmatter
     // parser is vfile-matter.
     if ("matter" in file.data && file.data.matter) {
