b79b55f292c97e007841a7718010a02c768af95d
Lucas <lucasnrgaard@gmail.com>
2025-02-08 07:41:33 +0100

    feat(examples): add Dockerfile and cleanup script; enhance blog post layout with estimation

--- examples/minimal-astro/astro.config.mjs
+++ examples/minimal-astro/astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkLesetid from "remark-lesetid/astro";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkLesetid]
+	}
 });
