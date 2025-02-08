a0ee1929ac9b6609b35cd7648da81ee82791a993
Lucas <lucasnrgaard@gmail.com>
2025-02-08 06:28:20 +0100

    chore: add debug

--- www/src/lib/texts.ts
+++ www/src/lib/texts.ts
@@ -1,8 +1,11 @@
 import { join } from "node:path";
 
 export async function getTexts() {
+  // eslint-disable-next-line no-console
+  console.log("BEFORE");
   const fileObjects = Object.values(import.meta.glob("../texts/*.md", { eager: true }));
-
+  // eslint-disable-next-line no-console
+  console.log("AFTER 1");
   const files = await Promise.all(fileObjects.map(async (fileModule) => {
     if (
       fileModule == null
@@ -16,6 +19,9 @@ export async function getTexts() {
     if (fileModule.file.endsWith("README.md")) {
       return null;
     }
+
+    // eslint-disable-next-line no-console
+    console.log("FILE", fileModule);
 
     return {
       file: fileModule.file.replace(join(import.meta.dirname, "../texts/"), ""),
